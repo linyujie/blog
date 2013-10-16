0d9603473431f6009674db7d039f3239140592a2
Gaubee <GaubeeBangeel@gmail.com>
2013-10-16 17:04:34 +0800

    Use Gmail添加邮件
    Signed-off-by: Gaubee <GaubeeBangeel@gmail.com>

--- config.js
+++ config.js
@@ -7,37 +7,45 @@ var path = require('path'),
 config = {
     // ### Development **(default)**
     development: {
-		// The url to use when providing links to the site, E.g. in RSS and email.
-		        url: 'http://my-ghost-blog.com',
+        // The url to use when providing links to the site, E.g. in RSS and email.
+        url: 'http://my-ghost-blog.com',
 
-		        // Example mail config
-		        // Visit http://docs.ghost.org/mail for instructions
-		        // ```
-		        //  mail: {
-		        //      transport: 'SMTP',
-		        //      options: {
-		        //          service: 'Mailgun',
-		        //          auth: {
-		        //              user: '', // mailgun username
-		        //              pass: ''  // mailgun password
-		        //          }
-		        //      }
-		        //  },
-		        // ```
-
-		        database: {
-		            client: 'sqlite3',
-		            connection: {
-		                filename: path.join(__dirname, '/content/data/ghost-dev.db')
-		            },
-		            debug: false
-		        },
-		        server: {
-		            // Host to be passed to node's `net.Server#listen()`
-		            host: '127.0.0.1',
-		            // Port to be passed to node's `net.Server#listen()`, for iisnode set this to `process.env.PORT`
-		            port: '2368'
-		        }
+        // Example mail config
+        // Visit http://docs.ghost.org/mail for instructions
+        // ```
+        //  mail: {
+        //      transport: 'SMTP',
+        //      options: {
+        //          service: 'Mailgun',
+        //          auth: {
+        //              user: '', // mailgun username
+        //              pass: ''  // mailgun password
+        //          }
+        //      }
+        //  },
+        // ```
+        mail: {
+            transport: 'SMTP',
+            options: {
+                auth: {
+                    user: 'GaubeeBlog@gmail.com', // mailgun username
+                    pass: 'gaubee546528' // mailgun password
+                }
+            }
+        },
+        database: {
+            client: 'sqlite3',
+            connection: {
+                filename: path.join(__dirname, '/content/data/ghost-dev.db')
+            },
+            debug: false
+        },
+        server: {
+            // Host to be passed to node's `net.Server#listen()`
+            host: '127.0.0.1',
+            // Port to be passed to node's `net.Server#listen()`, for iisnode set this to `process.env.PORT`
+            port: '2368'
+        }
     },
 
     // ### Production
